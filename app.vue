dfbc6fafa3de3a2eb9c9a70937ac7ec2434fe68c
Leonardo Bishop <me@leonardobishop.com>
2024-03-13 19:41:24 +0000

    Add favicon

--- app.vue
+++ app.vue
@@ -2,17 +2,36 @@
 import '~/assets/main.css'
 
 useHead({
-  link: [
-    {
-      rel: 'preconnect',
-      href: 'https://rsms.me/'
-    },
-    {
-      rel: 'stylesheet',
-      href: 'https://rsms.me/inter/inter.css',
-      crossorigin: ''
-    }
-  ]
+  link: [{
+    rel: 'preconnect',
+    href: 'https://rsms.me/'
+  }, {
+    rel: 'stylesheet',
+    href: 'https://rsms.me/inter/inter.css',
+    crossorigin: ''
+  }, {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/apple-touch-icon.png'
+  }, {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon-32x32.png'
+  }, {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon-16x16.png'
+  }, {
+    rel: 'manifest',
+    href: '/site.webmanifest'
+  }]
+})
+
+useSeoMeta({
+  title: 'Quests Web Editor',
+  ogTitle: 'Quests Web Editor',
 })
 </script>
 
