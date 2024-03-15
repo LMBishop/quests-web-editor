1dd5d0fa8bb2ae794b263d1629a662166a9b9d08
Leonardo Bishop <me@leonardobishop.com>
2024-03-15 00:11:39 +0000

    Update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,30 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   overrides: [
     {
       files: ['**/*.ts', '**/*.vue'],
       rules: {
-        'vue/multi-word-component-names': 'off'
-      }
-    }
-  ]
-}
+        'vue/multi-word-component-names': 'off',
+      },
+    },
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
